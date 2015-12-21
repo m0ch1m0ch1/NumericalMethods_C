5c52c737b522a545c64a14ab8b00e67de772415c
Motomura Yohei <y.motomura0426@gmail.com>
2015-12-21 16:37:32 +0900

    コマンドライン引数の動作テスト

--- Simpson/src/sample.c
+++ Simpson/src/sample.c
@@ -7,14 +7,17 @@ double func(double x){
   return sqrt(x);
 }
 
-int main(void){
+int main(int argc, char *argv[]){
 
   int i;
+
+  printf("%s\n", argv[0]);
 
   for (i = 1 ; i < 1.0E+06 ; i *= 2){
     printf("n = %8d \t", i );
     printf("Daikei  : %.15lf \t",  Daikei(i, 1.0 , 1.3));
     printf("Simpson : %.15lf \n", Simpson(i, 1.0 , 1.3));
   }
+
   return 0;
 }
